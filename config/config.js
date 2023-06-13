56419f0c5dfd9c5e108c97def795f31fae83ea7f
MrFabiioo <fabiolin57@gmail.com>
2023-06-13 12:25:13 -0500

    primer commit

---
+++ config/config.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const config = {
+  env : process.env.NODE_ENV || 'dev',
+  port: process.env.PORT || 3000,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+  dbPort: process.env.DB_PORT,
+  pgEmail: process.env.PG_EMAIL,
+  pgPassword: process.env.PG_PASSWORD,
+  pgPort: process.env.PG_PORT,
+  pgRefPort:process.env.PG_REFPORT,
+  apiKey:process.env.API_KEY,
+  jwtSecret:process.env.JWT_SECRET,
+  nodeApp:process.env.NODE_APP,
+  nodeMailesGmail:process.env.GMAIL,
+}
+
+module.exports = {config};
